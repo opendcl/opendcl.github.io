2b353c327034d2aee749cd667bdf6697e4acf3dd
Owen Wengerd <owenw@manusoft.com>
2026-09-07 20:48:39 -0400

    Put community links in static help HTML, not Utility.js.

--- HelpFiles/DEU/Utility.js
+++ HelpFiles/DEU/Utility.js
@@ -62,19 +62,3 @@ function copyclip(elem) {
   }
   return fallbackCopy(text);
 }
-
-(function () {
-  function setHdrlink() {
-    var el = document.getElementById("hdrlink");
-    if (!el) return;
-    el.innerHTML =
-      '<a href="https://www.opendcl.com/go?forum" target="_blank">Frage stellen</a><br />' +
-      '<a href="https://www.opendcl.com/go?wishlist&new" target="_blank">Wunschliste</a><br />' +
-      '<a href="https://www.opendcl.com/go?bugreport&new" target="_blank">Fehler melden</a>';
-  }
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", setHdrlink);
-  } else {
-    setHdrlink();
-  }
-})();
--- HelpFiles/ENU/Utility.js
+++ HelpFiles/ENU/Utility.js
@@ -62,19 +62,3 @@ function copyclip(elem) {
   }
   return fallbackCopy(text);
 }
-
-(function () {
-  function setHdrlink() {
-    var el = document.getElementById("hdrlink");
-    if (!el) return;
-    el.innerHTML =
-      '<a href="https://www.opendcl.com/go?forum" target="_blank">Ask a Question</a><br />' +
-      '<a href="https://www.opendcl.com/go?wishlist&new" target="_blank">Wishlist</a><br />' +
-      '<a href="https://www.opendcl.com/go?bugreport&new" target="_blank">Report a Bug</a>';
-  }
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", setHdrlink);
-  } else {
-    setHdrlink();
-  }
-})();
